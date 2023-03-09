120f7ed2dc99dfe6cb1bfc20940d7f580dbd9564
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2023-03-09 22:29:18 +0100

    avoided duplicate numbers + better the main number displaying

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,28 +11,42 @@ export class DashboardComponent implements OnInit {
   public streak: number = 0;
 
   ngOnInit(): void {
-    this.generatedNumber = this.generateNumber();
+    this.generatedNumber = this.generateNumber(this.generatedNumber);
+  }
+
+  public getNumberToDisplay = (): string => {
+    const currentNumberAsString = this.generatedNumber.toString();
+    const numberOfDigits = currentNumberAsString.length;
+
+    switch (numberOfDigits) {
+      case 1:
+        return `00` + currentNumberAsString;
+      case 2:
+        return `0` + currentNumberAsString;
+      default:
+        return currentNumberAsString;
+    }
   }
 
   public checkHigher = (): void => {
-    const newNumber = this.generateNumber();
     const oldNumber = this.generatedNumber;
+    const newNumber = this.generateNumber(oldNumber);
     this.generatedNumber = newNumber;
 
-    newNumber >= oldNumber ? this.streak++ : this.streak = 0;
+    newNumber > oldNumber ? this.streak++ : this.streak = 0;
 
   }
 
   public checkLower = (): void => {
-    const newNumber = this.generateNumber();
     const oldNumber = this.generatedNumber;
+    const newNumber = this.generateNumber(oldNumber);
     this.generatedNumber = newNumber;
 
-    newNumber <= oldNumber ? this.streak++ : this.streak = 0;
+    newNumber < oldNumber ? this.streak++ : this.streak = 0;
   }
 
-  private generateNumber = (): number => {
+  private generateNumber = (currentNumber: number): number => {
     const number = Math.floor(Math.random() * 1000);
-    return number === 1000 ? 999 : number;
+    return number === 1000 || number === currentNumber ? this.generateNumber(currentNumber) : number;
   };
 }
